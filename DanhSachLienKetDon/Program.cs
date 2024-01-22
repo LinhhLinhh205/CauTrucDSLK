584f77eda0a1555181cd700f969a5e6653ebb8f1
Linh <linhlinhbehy@gmail.com>
2024-01-22 09:24:58 +0700

    bo sung:tim max,tinhtb,xoa nut

--- DanhSachLienKetDon/Program.cs
+++ DanhSachLienKetDon/Program.cs
@@ -8,15 +8,15 @@ namespace DanhSachLienKetDon
 {
     class Node
     {
-        private int info;
+        private float info;
         private Node next;
 
-        public Node(int x)
+        public Node(float x)
         {
             info = x;
             next = null;
         }
-        public int Info
+        public float Info
         {
             get { return info; }
             set { info = value; }
@@ -25,6 +25,7 @@ namespace DanhSachLienKetDon
         {
             get { return next; }
             set { next = value; }
+
         }
     }
     class SingleLinkList
@@ -34,13 +35,13 @@ namespace DanhSachLienKetDon
         {
             Head = null;
         }
-        public void AddHead(int x)
+        public void AddHead(float x)
         {
             Node p = new Node(x);
             p.Next = Head;
             Head = p;
         }
-        public void AddLast(int x)
+        public void AddLast(float x)
         {
             Node p = new Node(x);
             if (Head == null)
@@ -76,6 +77,55 @@ namespace DanhSachLienKetDon
                 q.Next = null;
             }
         }
+        //tim max
+        public Node findMax()
+        {
+            Node max = Head;
+            Node p = Head.Next;
+            while (p != null)
+            {
+                if (p.Info > max.Info)
+                    max = p;
+                p = p.Next;
+            }
+            return max;
+        }
+        //tinh trung binh
+        public float Avg()
+        {
+            float sum = 0;
+            int count = 0;
+            Node p = Head;
+            while (p != null)
+            {
+                sum += p.Info;
+                count++;
+                p = p.Next;
+
+            }
+            return sum / count;
+        }
+        // xoa gia tri nut x
+        public void DeleteNode(float x)
+        {
+            Node p = Head;
+            Node q = null;
+            while (p != null && p.Info != x)
+            {
+                q = p;
+                p = p.Next;
+            }
+            if (p != null)
+            {
+                if (p == Head)
+                    DeleteHead();
+                else
+                {
+                    q.Next = p.Next;
+                    p.Next = null;
+                }
+            }
+        }
         public void ProcessList()
         {
             Node p = Head;
@@ -91,23 +141,38 @@ namespace DanhSachLienKetDon
         static void Main(string[] args)
         {
             SingleLinkList a = new SingleLinkList();
-            a.AddHead(1);
-            a.AddHead(3);
-            a.AddHead(9);
-            a.AddLast(5);
-            a.AddHead(7);
-            Console.WriteLine("Danh sach lien ket");
+            NhapDanhSach(a);
+            Console.WriteLine("Danh sach vua tao:");
             a.ProcessList();
-
-            a.DeleteHead();
-            Console.WriteLine("\nDanh sach lien ket sau khi xoa dau");
+            Console.WriteLine($"\nGia tri trung binh cua cac nut la:{a.Avg()}");
+            Node max = a.findMax();
+            Console.WriteLine($"\nNut co gia tri lon nhat:{max.Info}");
+            Console.Write("Nhap gia tri can xoa:");
+            float x = float.Parse(Console.ReadLine());
+            a.DeleteNode(x);
+            Console.WriteLine("\nDanh sach sau khi xoa:");
             a.ProcessList();
-
-            a.DeleteLast();
-            Console.WriteLine("\nDanh  sach lien ket sau khi xoa cuoi");
-            a.ProcessList();
-            Console.ReadKey();
+            Console.ReadLine();
         }
-        
+        static void NhapDanhSach(SingleLinkList l)
+        {
+
+            string chon = "y";
+            int x;
+            while (true)
+            {
+                Console.Write("Nhap gia tri nut:");
+                x = int.Parse(Console.ReadLine());
+                l.AddHead(x);
+                Console.Write("Tiep tuc y/n:");
+                chon = Console.ReadLine();
+                if (chon == "n")
+                    break;
+
+
+            }
+        }
     }
 }
+        
+ 
